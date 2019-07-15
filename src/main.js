1dafc9b1e0d0807673f47b5c8665e346b8d68b61
kuyedie <1055188318@qq.com>
2019-07-15 10:59:14 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入包
+import ElementUI from 'element-ui'
+// 导入样式
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+// 注册到VUE实例
+Vue.use(ElementUI)
+Vue.use(router)
 Vue.config.productionTip = false
-
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
